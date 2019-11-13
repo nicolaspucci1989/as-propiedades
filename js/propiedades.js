feb4be5bd17f2a36e0f431aad6ca4019024e8fe6
Nicolas Pucci <nicolaspucci1989@gmail.com>
2019-11-13 14:48:09 -0300

    moviendo porpiedades a su archivo

---
+++ js/propiedades.js
@@ -0,0 +1,61 @@
+const propiedades = [
+    {
+      id: 1,
+      imagen: '/img/as-prop/propiedades/001/casa.jose.001.jpg',
+      direccion: 'Libertad 420',
+      barrio: 'Villa Bosch',
+      tipo: 'Chalet',
+      valor: '170.000',
+      vendido: false,
+      etiquetas: ['3 amb.', '2 bañ.', 'cochera', '200 m² total']
+    },
+    {
+      id: 2,
+      imagen: '/img/as-prop/propiedades/001/casa.jose.002.jpg',
+      direccion: 'San Martin 1337',
+      barrio: 'Palomar',
+      tipo: 'Casa',
+      valor: '120.000',
+      vendido: true,
+      etiquetas: ['4 amb.', '3 bañ.', 'jardin botanico', '400 m² total']
+    },
+    {
+      id: 3,
+      imagen: '/img/as-prop/propiedades/001/casa.jose.003.jpg',
+      direccion: 'Cordoba 240',
+      barrio: 'Ballester',
+      tipo: 'PH',
+      valor: '200.000',
+      vendido: false,
+      etiquetas: ['3 amb.', '2 bañ.', 'cochera', '200 m² total']
+    },
+    {
+      id: 4,
+      imagen: '/img/as-prop/propiedades/001/casa.jose.004.jpg',
+      direccion: 'Rosales 101',
+      barrio: 'Moron',
+      tipo: 'Departamento',
+      valor: '220.000',
+      vendido: true,
+      etiquetas: ['3 amb.', 'cochera', '200 m² total']
+    },
+    {
+      id: 5,
+      imagen: '/img/as-prop/propiedades/001/casa.jose.004.jpg',
+      direccion: 'Rosales 102',
+      barrio: 'Moron',
+      tipo: 'Departamento',
+      valor: '220.000',
+      vendido: true,
+      etiquetas: ['3 amb.', '2 bañ.', '200 m² total']
+    },
+    {
+      id: 6,
+      imagen: '/img/as-prop/propiedades/001/casa.jose.004.jpg',
+      direccion: 'Rosales 103',
+      barrio: 'Moron',
+      tipo: 'Departamento',
+      valor: '220.000',
+      vendido: false,
+      etiquetas: ['3 amb.', '2 bañ.', 'cochera', '200 m² total']
+    }];
