7be087773e586453e09b31153524bae86fed7cef
Nicolas Pucci <nicolaspucci1989@gmail.com>
2019-11-19 12:41:39 -0300

    agregando props

--- js/propiedades.js
+++ js/propiedades.js
@@ -7,7 +7,9 @@ const propiedades = [
       tipo: 'Chalet',
       valor: '170.000',
       vendido: false,
-      etiquetas: ['3 amb.', '2 bañ.', 'cochera', '200 m² total']
+      etiquetas: ['3 amb.', '2 bañ.', 'cochera', '200 m² total'],
+      descripcion: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione culpa voluptate officia, qui doloribus vitae necessitatibus. Pariatur ipsum reprehenderit, dolore, alias architecto modi consequuntur, minima exercitationem accusantium quisquam quo soluta.",
+      zona: 'G.B.A Zona Oeste'
     },
     {
       id: 2,
@@ -17,7 +19,9 @@ const propiedades = [
       tipo: 'Casa',
       valor: '120.000',
       vendido: true,
-      etiquetas: ['4 amb.', '3 bañ.', 'jardin botanico', '400 m² total']
+      etiquetas: ['4 amb.', '3 bañ.', 'jardin botanico', '400 m² total'],
+      descripcion: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione culpa voluptate officia, qui doloribus vitae necessitatibus. Pariatur ipsum reprehenderit, dolore, alias architecto modi consequuntur, minima exercitationem accusantium quisquam quo soluta.",
+      zona: 'G.B.A Zona Oeste'
     },
     {
       id: 3,
@@ -27,7 +31,9 @@ const propiedades = [
       tipo: 'PH',
       valor: '200.000',
       vendido: false,
-      etiquetas: ['3 amb.', '2 bañ.', 'cochera', '200 m² total']
+      etiquetas: ['3 amb.', '2 bañ.', 'cochera', '200 m² total'],
+      descripcion: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione culpa voluptate officia, qui doloribus vitae necessitatibus. Pariatur ipsum reprehenderit, dolore, alias architecto modi consequuntur, minima exercitationem accusantium quisquam quo soluta.",
+      zona: 'G.B.A Zona Oeste'
     },
     {
       id: 4,
@@ -37,7 +43,9 @@ const propiedades = [
       tipo: 'Departamento',
       valor: '220.000',
       vendido: true,
-      etiquetas: ['3 amb.', 'cochera', '200 m² total']
+      etiquetas: ['3 amb.', 'cochera', '200 m² total'],
+      descripcion: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione culpa voluptate officia, qui doloribus vitae necessitatibus. Pariatur ipsum reprehenderit, dolore, alias architecto modi consequuntur, minima exercitationem accusantium quisquam quo soluta.",
+      zona: 'G.B.A Zona Oeste'
     },
     {
       id: 5,
@@ -47,7 +55,9 @@ const propiedades = [
       tipo: 'Departamento',
       valor: '220.000',
       vendido: true,
-      etiquetas: ['3 amb.', '2 bañ.', '200 m² total']
+      etiquetas: ['3 amb.', '2 bañ.', '200 m² total'],
+      descripcion: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione culpa voluptate officia, qui doloribus vitae necessitatibus. Pariatur ipsum reprehenderit, dolore, alias architecto modi consequuntur, minima exercitationem accusantium quisquam quo soluta.",
+      zona: 'G.B.A Zona Oeste'
     },
     {
       id: 6,
@@ -57,5 +67,7 @@ const propiedades = [
       tipo: 'Departamento',
       valor: '220.000',
       vendido: false,
-      etiquetas: ['3 amb.', '2 bañ.', 'cochera', '200 m² total']
+      etiquetas: ['3 amb.', '2 bañ.', 'cochera', '200 m² total'],
+      descripcion: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione culpa voluptate officia, qui doloribus vitae necessitatibus. Pariatur ipsum reprehenderit, dolore, alias architecto modi consequuntur, minima exercitationem accusantium quisquam quo soluta.",
+      zona: 'G.B.A Zona Oeste'
     }];
